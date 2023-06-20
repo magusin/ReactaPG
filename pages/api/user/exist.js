06a838fb73c90ff9e7c228af648cbeb81bae6b1e
magusin <fanatsy4@gmail.com>
2023-06-21 01:28:55 +0200

    add api/user/exist

---
+++ pages/api/user/exist.js
@@ -0,0 +1,23 @@
+import connection from '../../../db';
+
+export default async function handler(req, res) {
+    switch (req.method) {
+      case "POST":
+        return playerExist(req, res);
+      default:
+        return res.status(400).json({ message: "bad request" });
+    }
+  }
+
+  const playerExist = async (req, res) => {
+    try {
+        connection.query('SELECT * FROM player WHERE name = ?', [req.body.username], (error, results) => {
+            if (error) throw error;
+            console.log(results)
+            return res.status(200).json(results);
+        })
+        
+      } catch (error) {
+        return res.status(500).json({ message: error.message });
+      }
+    };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,10 +2,12 @@
 'use client'
 import { useState, useEffect } from 'react'
 import { TextField, Button, Box, Container } from '@mui/material'
+import axios from 'axios'
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
   const [log, setLog] = useState(true)
 
   const handleLogin = () => {
@@ -13,6 +15,19 @@ const Login: React.FC = () => {
     console.log('Email:', email)
     console.log('Password:', password)
   }
+
+const handleTest = async () => {
+  try {
+    const response = await axios.post('http://localhost:3000/api/user/exist', {
+      username: 'test'
+    })
+    console.log('Data test:', response.data)
+  } catch (error) {
+    console.error('error', error)
+  }
+}
+
+handleTest()
 
   const handleRegister = () => {
     // Logique d'enregistrement ici
@@ -57,6 +72,12 @@ const Login: React.FC = () => {
               marginTop: '200px'
             }}
           >
+            <TextField
+              label="Username"
+              type="username"
+              value={username}
+              style={{}}
+            />
             <TextField label="Email" type="email" value={email} style={{}} />
             <TextField
               label="Password"
@@ -70,9 +91,11 @@ const Login: React.FC = () => {
             <Button variant="text" onClick={() => setLog(!log)}>J'ai déjà un compte</Button>
           </Box>
         </Container>
+        
       )}
     </>
   )
 }
+
 
 export default Login
