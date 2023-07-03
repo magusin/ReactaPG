4731d0e775fd833637fd88582e40dc017165b4ff
magusin <fanatsy4@gmail.com>
2023-07-04 00:03:55 +0200

    delete isLoggedIn func utils, add const user, fetch data allPlayers

--- src/app/duel/page.tsx
+++ src/app/duel/page.tsx
@@ -2,51 +2,81 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import {
-    Box,
-    Container,
-    Typography,
-    Button
-  } from '@mui/material'
+  Box,
+  Container,
+  Typography,
+  Button,
+  CircularProgress
+} from '@mui/material'
 import Header from 'src/components/header'
 import axios from 'axios'
 import { Player } from 'src/types/Player'
+import { useRouter } from 'next/navigation'
 
 export default function Duel() {
-    const [players, setPlayers] = useState<Player[]>([]);
+  const [players, setPlayers] = useState<Player[]>([])
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            // Remplacer ces endpoints par les vôtres
-            const player = await axios.get('/api/user');
-            console.log(player)
-            setPlayers(player.data);
-          } catch (err) {
-            console.error(err);
-          }
-        };
-    
-        fetchData();
-        
-      }, []);
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+    setUser(user);
+    if (user != null) {
+      const fetchData = async () => {
+        try {
+          // call api allPlayers
+          await axios
+            .get('/api/user')
+            .then((res) => {
+              setPlayers(res.data)
+            })
+            .finally(() => {
+              setIsLoading(false)
+            })
+        } catch (err) {
+          console.error(err)
+        }
+      }
+      fetchData()  
+    } else {
+      // Redirect to login page if user is not logged in
+      router.push('/login') 
+    }
+  }, [user, router])
 
+  if (isLoading) {
     return (
-        <>
-        <Header />
-        <Container>
-          <Typography variant="h4" gutterBottom>
-            Choisissez votre adversaire
-          </Typography>
-    
-          {/* {opponents.map((player) => (
-            <Box key={player.id} marginBottom={2}>
-              <Typography variant="h6">{player.name}</Typography>
-              <Button variant="contained" color="primary">
-                Duel
-              </Button>
-            </Box>
-          ))} */}
-        </Container>
-        </>
-      );
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh'
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    )
+  }
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <Typography variant="h4" gutterBottom>
+          Choisissez votre adversaire
+        </Typography>
+
+        {players.map((player) => (
+          <Box key={player.id} marginBottom={2}>
+            <Typography variant="h6">{player.username}</Typography>
+            <Button variant="contained" color="primary">
+              Duel
+            </Button>
+          </Box>
+        ))}
+      </Container>
+    </>
+  )
 }
