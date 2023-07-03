c9a5e9622c1f03dd45bf42493f47299e608810ff
magusin <fanatsy4@gmail.com>
2023-07-04 01:44:02 +0200

    logic for random opponent

--- src/app/duel/page.tsx
+++ src/app/duel/page.tsx
@@ -23,27 +23,45 @@ export default function Duel() {
     const user = localStorage.getItem('user');
     setUser(user);
     if (user != null) {
+      const userId = JSON.parse(user).id;
+      
       const fetchData = async () => {
         try {
-          // call api allPlayers
-          await axios
-            .get('/api/user')
-            .then((res) => {
-              setPlayers(res.data)
-            })
-            .finally(() => {
-              setIsLoading(false)
-            })
+          const res = await axios.get('/api/user')
+          let currentPlayer = res.data.filter((player) => player.id === userId)[0];
+          const userLevel = currentPlayer.level;
+          let allPlayers = res.data.filter((player) => player.id !== userId); // Exclude current user
+          let filteredPlayers = [];
+          let currentLevel = userLevel;
+          console.log(currentLevel)
+          while (filteredPlayers.length < 6 && currentLevel < 100) {
+            // Get players of current level
+            let playersOfCurrentLevel = allPlayers.filter((player) => player.level === currentLevel);
+            console.log('playersOfCurrentLevel', playersOfCurrentLevel)
+            // Shuffle the array
+            for (let i = playersOfCurrentLevel.length - 1; i > 0; i--) {
+              const j = Math.floor(Math.random() * (i + 1));
+              [playersOfCurrentLevel[i], playersOfCurrentLevel[j]] = [playersOfCurrentLevel[j], playersOfCurrentLevel[i]];
+            }
+            
+            // Add to filteredPlayers
+            filteredPlayers = [...filteredPlayers, ...playersOfCurrentLevel].slice(0, 6);
+            console.log('filteredPlayers:', filteredPlayers)
+            currentLevel++;
+          }
+          console.log(filteredPlayers)
+          setPlayers(filteredPlayers);
+          setIsLoading(false);
         } catch (err) {
-          console.error(err)
+          console.error(err);
         }
-      }
-      fetchData()  
+      };
+      fetchData();
     } else {
       // Redirect to login page if user is not logged in
-      router.push('/login') 
+      router.push('/login');
     }
-  }, [user, router])
+  }, [user, router]);
 
   if (isLoading) {
     return (
