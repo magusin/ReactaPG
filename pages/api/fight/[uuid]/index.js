67b0b872e4117caf385d890a2bc7c18a83525adf
magusin <fanatsy4@gmail.com>
2023-07-13 12:15:23 +0200

    add api/fight/[uuid], get one fight and all fightEvent linked

--- pages/api/fight/[uuid]/index.js
+++ pages/api/fight/[uuid]/index.js
@@ -1,12 +1,12 @@
-import Cors from 'cors'
 import { PrismaClient } from '@prisma/client'
+import Cors from 'cors'
+
+const prisma = new PrismaClient()
 
 // Initialiser le middleware CORS
 let cors = Cors({
-  methods: ['POST', 'HEAD']
+  methods: ['GET', 'HEAD']
 })
-
-const prisma = new PrismaClient()
 
 function runMiddleware(req, res, fn) {
   return new Promise((resolve, reject) => {
@@ -14,7 +14,6 @@ function runMiddleware(req, res, fn) {
       if (result instanceof Error) {
         return reject(result)
       }
-
       return resolve(result)
     })
   })
@@ -22,29 +21,32 @@ function runMiddleware(req, res, fn) {
 
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors)
+  
   switch (req.method) {
     case 'GET':
-      if (req.query.uuid) {
-        return await getPlayerFights(req, res)
-      }
-      break
+      return getFight(req, res)
     default:
-      return res.status(400).json({ message: 'bad request' })
+      return res.status(405).end(`Method ${req.method} Not Allowed`)
   }
 }
 
-const getPlayerFights = async (req, res) => {
+const getFight = async (req, res) => {
   try {
     const { uuid } = req.query
 
     const fight = await prisma.fight.findUnique({
-      where: {
-        uuid: uuid
-      }
+      where: { uuid: uuid },
+      include: {
+        events: true,
+      },
     })
+
+    if (!fight) {
+      return res.status(404).json({ error: 'Fight not found' })
+    }
 
     return res.status(200).json(fight)
   } catch (error) {
-    return res.status(500).json({ message: error.message })
+    return res.status(500).json({ error: error.message })
   }
 }
