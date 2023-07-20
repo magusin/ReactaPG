f260b33d44f09cc6b746b36522d6b9830620424e
magusin <fanatsy4@gmail.com>
2023-07-21 01:52:32 +0200

    add security multi instance prisma and deleted user redirection

--- prisma/migrations/20230719140658_add_abilities/migration.sql
+++ prisma/migrations/20230719140658_add_abilities/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE `Ability` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `strengthIncrease` INTEGER NOT NULL DEFAULT 0,
-    `agilityIncrease` INTEGER NOT NULL DEFAULT 0,
+    `dexterityIncrease` INTEGER NOT NULL DEFAULT 0,
     `healthIncrease` INTEGER NOT NULL DEFAULT 0,
     `speedIncrease` INTEGER NOT NULL DEFAULT 0,
     `image` VARCHAR(191) NOT NULL,
--- prisma/prisma-seedsAbility.js
+++ prisma/prisma-seedsAbility.js
@@ -4,18 +4,18 @@ const prisma = new PrismaClient();
 
 async function main() {
   const abilities = [
-    { name: "Puissant", strengthIncrease: 2, agilityIncrease: 1, healthIncrease: 0, speedIncrease: 0 },
-    { name: "Robuste", strengthIncrease: 2, agilityIncrease: 0, healthIncrease: 3, speedIncrease: 0 },
-    { name: "Combatif", strengthIncrease: 2, agilityIncrease: 0, healthIncrease: 0, speedIncrease: 1 },
-    { name: "Adroit", strengthIncrease: 1, agilityIncrease: 2, healthIncrease: 0, speedIncrease: 0 },
-    { name: "Ingénieux", strengthIncrease: 0, agilityIncrease: 2, healthIncrease: 3, speedIncrease: 0 },
-    { name: "Habile", strengthIncrease: 0, agilityIncrease: 2, healthIncrease: 0, speedIncrease: 1 },
-    { name: "Tenace", strengthIncrease: 1, agilityIncrease: 0, healthIncrease: 6, speedIncrease: 0},
-    { name: "Infatigable", strengthIncrease: 0, agilityIncrease: 1, healthIncrease: 6, speedIncrease: 0 },
-    { name: "Vigoureux", strengthIncrease: 0, agilityIncrease: 0, healthIncrease: 6, speedIncrease: 1 },
-    { name: "Energique", strengthIncrease: 1, agilityIncrease: 0, healthIncrease: 0, speedIncrease: 2 },
-    { name: "Vivace", strengthIncrease: 0, agilityIncrease: 1, healthIncrease: 0, speedIncrease: 2 },
-    { name: "Dynamique", strengthIncrease: 0, agilityIncrease: 0, healthIncrease: 3, speedIncrease: 2}
+    { name: "Puissant", strengthIncrease: 2, dexterityIncrease: 1, healthIncrease: 0, speedIncrease: 0 },
+    { name: "Robuste", strengthIncrease: 2, dexterityIncrease: 0, healthIncrease: 3, speedIncrease: 0 },
+    { name: "Combatif", strengthIncrease: 2, dexterityIncrease: 0, healthIncrease: 0, speedIncrease: 1 },
+    { name: "Adroit", strengthIncrease: 1, dexterityIncrease: 2, healthIncrease: 0, speedIncrease: 0 },
+    { name: "Ingénieux", strengthIncrease: 0, dexterityIncrease: 2, healthIncrease: 3, speedIncrease: 0 },
+    { name: "Habile", strengthIncrease: 0, dexterityIncrease: 2, healthIncrease: 0, speedIncrease: 1 },
+    { name: "Tenace", strengthIncrease: 1, dexterityIncrease: 0, healthIncrease: 6, speedIncrease: 0},
+    { name: "Infatigable", strengthIncrease: 0, dexterityIncrease: 1, healthIncrease: 6, speedIncrease: 0 },
+    { name: "Vigoureux", strengthIncrease: 0, dexterityIncrease: 0, healthIncrease: 6, speedIncrease: 1 },
+    { name: "Energique", strengthIncrease: 1, dexterityIncrease: 0, healthIncrease: 0, speedIncrease: 2 },
+    { name: "Vivace", strengthIncrease: 0, dexterityIncrease: 1, healthIncrease: 0, speedIncrease: 2 },
+    { name: "Dynamique", strengthIncrease: 0, dexterityIncrease: 0, healthIncrease: 3, speedIncrease: 2}
   ];
 
   for (const ability of abilities) {
@@ -32,4 +32,5 @@ main()
   })
   .finally(async () => {
     await prisma.$disconnect();
+    console.log('seeds completed')
   });
