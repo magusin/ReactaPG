7823656828b44cce2c14fa37dc067a8220dc68a2
magusin <fanatsy4@gmail.com>
2023-08-20 03:44:02 +0200

    add skills

--- prisma/prisma-seedsSkill.js
+++ prisma/prisma-seedsSkill.js
@@ -4,9 +4,17 @@ const prisma = new PrismaClient();
 
 async function main() {
     const skills = [
-        { name: 'Renaissance', description: "Vous n'abandonnez jamais et vous relevez avec un dixième de vos points de vie maximum après avoir été réduit à 0 point de vie. Vous ne pouvez utiliser cette capacité qu'une fois par combat." },
-        { name: "Parade", description: "Si votre esquive échoue vous avez une chance de parer l'attaque et de réduire les dégâts de moitié." },
-        { name: "Fourberie", description: "Le fairplay c'est pour les faibles. Vous attaquez toujours en premier et avez une faible chance de la faire quand ce n'est pas votre tour."}
+        { name: "Renaissance", description: "Après avoir été réduit à 0 point de vie vous en regagnez un dixième (qu'une fois par combat)" },
+        { name: "Parade", description: "Si votre esquive échoue vous avez une chance de parer l'attaque et de réduire les dégâts reçus de moitié." },
+        { name: "Riposte", description: "Vous avez une chance de contre-attaquer après une attaque ennemie." },
+        { name: "Nociception", description: "Vous avez une chance de réduire les dégats reçus."},
+        { name: "Pyromancie", description: "Vous avez une chance de brûler votre adversaire et de lui infliger des dégats supplémentaires."},
+        { name: "Cryomancie", description: "Vous avez une chance de geler votre adversaire et de lui gêner son prochain tour."},
+        { name: "Thermoception", description: "Vous avez une chance d'annuler les effets liés aux éléments."},
+        { name: "Régénération", description: "Vous avez une chance de récupérer des points de vie."},
+        { name: "Rage", description: "Vous avez une chance d'augmentez votre force après avoir reçus un coup."}
+
+        
     ];
 
     for (const skill of skills) {
